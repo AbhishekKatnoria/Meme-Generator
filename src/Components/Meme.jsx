aa157b2d8f3287e58f775f6ad1c929de4bc9d949
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2024-01-03 18:15:24 +0530

    add some css

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,39 +1,69 @@
 import React, { useState } from 'react';
 import memeData from '../memeData';
+import './Meme.css';
 
 
 
 function Meme() {
-  const [meme,setMeme]=useState({
-    topText:"",
-    bottonText:"",
-    randowImage:"http://i.imgflip.com/1bij.jpg"
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottonText: "",
+    randowImage: "http://i.imgflip.com/1bij.jpg"
 
   })
 
-  const [memeImg,setMemeAllImg]=useState(memeData);
+  const [memeImg, setMemeAllImg] = useState(memeData);
 
 
   function handleClick() {
     const memeArray = memeData.data.memes;
     const randomNumber = Math.floor(Math.random() * memeArray.length);
-    const url=memeArray[randomNumber].url;
-      setMeme(prevMeme=>({
-           ...prevMeme,
-          randowImage:url  
-      }))
+    const url = memeArray[randomNumber].url;
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      randowImage: url
+
+
+    }))
   }
+
+    function handle(event) {
+      const { name, value } = event.target;
+      setMeme(prev => ({
+        ...prev,
+        [name]: value
+      }))
+    }
+  
 
   return (
     <>
       <main className='p-10'>
         <div className='w-full grid grid-cols-2 gap-10 '>
-          <input type="text" placeholder='Shut up' className='border border-black rounded-md py-2 ' />
-          <input type='text' placeholder='and take my money' className='border border-black rounded-md py-2' />
+          <input type="text"
+            placeholder='Shut up'
+            className='border border-black rounded-md py-2 '
+            name='topText'
+            value={meme.topText}
+            onChange={handle} />
+
+          <input type='text'
+            placeholder='and take my money'
+            className='border border-black rounded-md py-2'
+            name='bottonText'
+            value={meme.bottonText}
+            onChange={handle} />
+
+
           <button type='submit' onClick={handleClick} className='bg-purple-800 text-white  py-2  col-span-full rounded'>Get a new image meme</button>
         </div>
       </main>
-        <img src={meme.randowImage} className="text-center  "/> 
+      <div className='relative'>
+      <img src={meme.randowImage} />
+      <h2 className="meme--text top">{meme.topText}</h2>
+      <h2 className="meme--text bottom">{meme.bottonText}</h2>
+      </div>
+   
     </>
   )
 }
